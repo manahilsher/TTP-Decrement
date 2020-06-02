624e2fe8f753b7a1f6b767deb4c7eebe00eed228
Manahil Sher <manahil.sher@gmail.com>
2020-06-02 16:22:00 -0400

    completed decrement

---
+++ src/components/Decrement.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class Decrement extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      number: this.props.start,
+    };
+  }
+
+  handleClick = () => {
+    if (this.state.number > 0) {
+      this.setState({ number: this.state.number - 1 });
+    } else {
+      alert("Cannot be less than zero");
+    }
+  };
+
+  render() {
+    return (
+      <>
+        <h3>{this.state.number}</h3>
+        <button className="btn btn-primary" onClick={this.handleClick}>
+          Decrement
+        </button>
+      </>
+    );
+  }
+}
+
+Decrement.propTypes = { name: PropTypes.number.isRequired };
+
+export default Decrement;
